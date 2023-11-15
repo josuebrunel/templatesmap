7ea23685ac4d855f9d6a0b3244515ba04fc6224a
Josue Kouka <josuebrunel@gmail.com>
2023-11-14 16:40:05 -0800

    add templatesmap implementation and tests

---
+++ templatesmap.go
@@ -0,0 +1,55 @@
+// Package templatesmap provides functionality for managing HTML templates.
+package templatesmap
+
+import (
+	"errors"
+	"html/template" // For handling HTML templates.
+	"io"            // Used for input/output operations.
+	"path/filepath" // For handling file paths.
+)
+
+// ErrTemplateNotFound is the error returned when a requested template cannot be found.
+var ErrTemplateNotFound = errors.New("template not found")
+
+// TemplatesMap holds mappings from template names to their corresponding HTML template.
+type TemplatesMap struct {
+	Layouts   []string                      // Layouts is a list of layout template file paths.
+	Templates map[string]*template.Template // Templates is a map from template names to their parsed representations.
+}
+
+// Render attempts to write a rendered template to the provided writer.
+// It returns an error if the template is not found or if there's an issue during rendering.
+func (t TemplatesMap) Render(wr io.Writer, name string, data any) error {
+	tpl, ok := t.Templates[name]
+	if !ok {
+		return ErrTemplateNotFound
+	}
+	return tpl.ExecuteTemplate(wr, name, data)
+}
+
+// NewTemplatesMap initializes a new TemplatesMap with the given layout and page paths.
+// It returns a pointer to a TemplatesMap and an error if any occurs during initialization.
+func NewTemplatesMap(layoutPath string, pagesPath ...string) (*TemplatesMap, error) {
+	layouts, err := filepath.Glob(layoutPath) // Retrieve layout file paths.
+	if err != nil {
+		return nil, err
+	}
+
+	templates := make(map[string]*template.Template)
+	for _, l := range layouts {
+		templates[filepath.Base(l)] = template.Must(template.ParseFiles(layouts...))
+	}
+
+	for _, path := range pagesPath {
+		pages, err := filepath.Glob(path) // Retrieve page file paths.
+		if err != nil {
+			return nil, err
+		}
+
+		for _, page := range pages {
+			files := append(layouts, page)
+			templates[filepath.Base(page)] = template.Must(template.ParseFiles(files...))
+		}
+	}
+	return &TemplatesMap{Layouts: layouts, Templates: templates}, nil
+}
---
+++ templatesmap_test.go
@@ -0,0 +1,83 @@
+package templatesmap
+
+import (
+	"bytes"
+	"errors"
+	"html/template"
+	"log"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func assert(t *testing.T, left, right any) {
+	_, f, l, _ := runtime.Caller(1)
+	if !reflect.DeepEqual(left, right) {
+		log.Fatalf("[FAILURE](%s:%d) - %v != %v", filepath.Base(f), l, left, right)
+	}
+}
+
+func TestTemplatesMap(t *testing.T) {
+	var (
+		layoutsPath = "./testdata/layouts/*.html"
+		rolesPath   = "./testdata/roles/*.html"
+		usersPath   = "./testdata/users/*.html"
+	)
+
+	_, err := NewTemplatesMap(layoutsPath, "notfound")
+	assert(t, err, nil)
+
+	tpl, err := NewTemplatesMap("layouts", "notfound")
+	assert(t, err, nil)
+
+	t.Run("Template", func(t *testing.T) {
+		tpl, err = NewTemplatesMap(layoutsPath, rolesPath)
+		assert(t, err, nil)
+		assert(t, len(tpl.Layouts), 3)
+		log.Printf("templates: %v", tpl.Templates)
+		assert(t, len(tpl.Templates), 4)
+		_, ok := tpl.Templates["role-list.html"]
+		assert(t, ok, true)
+		user := template.Must(template.ParseGlob(usersPath))
+		tpl.Templates["user-detail"] = user
+		tpl.Templates["user-detail.html"] = user
+		assert(t, len(tpl.Templates), 6)
+		delete(tpl.Templates, "user-detail")
+		log.Printf("templates: %v", tpl.Templates)
+		assert(t, len(tpl.Templates), 5)
+	})
+	t.Run("Render", func(t *testing.T) {
+		tpl, err = NewTemplatesMap(layoutsPath, rolesPath, usersPath)
+		assert(t, err, nil)
+		buf := bytes.NewBufferString("")
+		err = tpl.Render(buf, "notfound", nil)
+		assert(t, errors.Is(err, ErrTemplateNotFound), true)
+		tpl.Render(buf, "base.html", nil)
+		log.Print(buf.String())
+		assert(t, strings.Contains(buf.String(), "base block"), true)
+		assert(t, strings.Contains(buf.String(), "example.css"), true)
+		assert(t, strings.Contains(buf.String(), "example.js"), true)
+		buf.Reset()
+		roles := []Role{{Name: "RoleA"}, {Name: "RoleB"}}
+		tpl.Render(buf, "role-list.html", roles)
+		log.Print(buf.String())
+		for _, r := range roles {
+			assert(t, strings.Contains(buf.String(), r.Name), true)
+		}
+		buf.Reset()
+		u := User{Email: "user@example.com"}
+		tpl.Render(buf, "user-detail.html", u)
+		log.Print(buf.String())
+		assert(t, strings.Contains(buf.String(), u.Email), true)
+	})
+}
+
+type User struct {
+	Email string
+}
+
+type Role struct {
+	Name string
+}
